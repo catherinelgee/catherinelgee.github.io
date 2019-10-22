35b34f0de24594e47310785554e52ef1fde19b09
Catherine Gee <catherinelgee@gmail.com>
2019-10-22 03:42:00 -0700

    webtris setup

--- webtris/webtris.js
+++ webtris/webtris.js
@@ -1,39 +1,40 @@
 // have a grid that is true/false (filled or not filled)
 // if the whole line is full then get rid of the line
 //
-//TODO DAS??
-function begin() {
-    board.start(); //set up the board
-    while (True) {
-        //generate a piece
-    }
+// TODO DAS?? 10 wide 20 high
+
+// needs: opening screen (choose level)
+// then play screen needs: points (todo lookup)
+// longterm maybe get some stats lol
+
+// 60 fps
+
+var grid = [];
+
+for (var i = 0;i < 20;i++) {
+     grid[i] = [];
 }
 
-var board = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 270;
-        this.canvas.height = 480;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+
+
+function drawPiece() {
+
 }
 
-var piece(shape, x, y, font) {
-    //i think shape will be like a string of what it's supposed to look like ??
-    this.update = function() {
-        // update location (redraw)
-    }
-    this.collision = function() {
-        // cant run into blocks that are right or left to it
-    }
-    this.stop = function() {
-        // at stop stop moving
+function drawGrid() {
+// draws the output of the grid (?)
+}
 
-    }
+function createWorld() {
+    // fills the grid with false values
+}
+
+function drawOpening() {
+    // draws the opening title
+}
+
+function draw() {
+    // the actual game
+    drawOpening();
+
 }
