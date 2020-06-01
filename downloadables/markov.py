93bf0ae8ac7744882cc7829dd0a6e75cb9eb4455
Catherine Gee <catherinelgee@gmail.com>
2020-06-01 02:00:44 -0700

    update desc, start some stuff with projects

---
+++ downloadables/markov.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+#markov chains for fun with text analysis
+
+### UI
+class app(tk.Frame):
+    """Sets up the tk object"""
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def widgets():
+
+    def end():
+        """Destroys itself"""
+        self.parent.destroy()
+### The chaining.
+
+
+
+
+### run
+app = tk.tk()
+app.mainloop()
