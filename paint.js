ec8d1f0f8b50e53cb774d37b6cd55cb222062716
Catherine Gee <catherinelgee@gmail.com>
2020-06-24 01:56:29 -0700

    i have no idea what im going (messing w javascript)

---
+++ paint.js
@@ -0,0 +1,47 @@
+var screenWidth;
+var screenHeight;
+
+var canvas;
+var context;
+var mouse;
+var mouseX;
+var mouseY;
+var down = false;
+
+function init()
+{
+    canvas = document.getElementById("myCanvas");
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    context = canvas.getContext("2d");
+    screenWidth = canvas.width;
+    screenHeight = canvas.height;
+
+
+
+    context.font = "48px Roboto";
+    context.textAlign = "center";
+    context.fillText("PAINT THE SITE", screenWidth / 2, (screenHeight / 2) - 30);
+
+    context.font = "36px Roboto";
+    context.textAlign = "center";
+    context.fillText("click to start painting", screenWidth / 2, (screenHeight / 2));
+
+    run();
+}
+
+function run() {
+    while (true) {
+        canvas.addEventListener('mousedown', function(e) {
+            getCursorPosition(canvas, e)
+        });
+        canvas.fillRect(mouseX, mouseY, 1, 1);
+    }
+
+}
+
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    mouseX = event.clientX - rect.left
+    mouseY = event.clientY - rect.top
+}
