3db93757f324d9b97297ad7c59f675ebcced14bb
Catherine Gee <catherinelgee@gmail.com>
2020-06-24 02:32:08 -0700

    bad paint

--- paint.js
+++ paint.js
@@ -8,44 +8,51 @@ var mouseX;
 var mouseY;
 var down = false;
 
-function init()
-{
-    canvas = document.getElementById("myCanvas");
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-    canvas.style.width = document.body.clientWidth;
-    canvas.style.height = document.body.clientHeight;
-    context = canvas.getContext("2d");
-    screenWidth = canvas.width;
-    screenHeight = canvas.height;
+function init() {
+  canvas = document.getElementById("myCanvas");
+  canvas.width = document.body.clientWidth;
+  canvas.height = document.body.clientHeight;
+  canvas.style.width = document.body.clientWidth;
+  canvas.style.height = document.body.clientHeight;
+  context = canvas.getContext("2d");
+  screenWidth = canvas.width;
+  screenHeight = canvas.height;
 
-    context.font = "48px Roboto";
-    context.textAlign = "center";
-    context.fillText("PAINT THE SITE", screenWidth / 2, (screenHeight / 2) - 30);
+  context.font = "48px Roboto";
+  context.textAlign = "center";
+  context.fillText("PAINT THE SITE", screenWidth / 2, screenHeight / 2 - 30);
 
-    context.font = "36px Roboto";
-    context.textAlign = "center";
-    context.fillText("click to start painting", screenWidth / 2, (screenHeight / 2));
+  context.font = "36px Roboto";
+  context.textAlign = "center";
+  context.fillText(
+    "click to start painting",
+    screenWidth / 2,
+    screenHeight / 2
+  );
 
-    run();
+  run();
 }
 
 function run() {
-    while (true) {
-        canvas.addEventListener('mousedown', function(e) {
-            getCursorPosition(canvas, e)
-        });
-        context.fillRect(mouseX, mouseY, 1, 1);
-    }
+  canvas.addEventListener("mousedown", isDown);
+  canvas.addEventListener("mousemove", function(e) {
+    getCursorPosition(canvas, e);
+  });
+  canvas.addEventListener("mouseup", isDown); //this might not work
+}
 
+function isDown() {
+  down = !down;
 }
 
 function getCursorPosition(canvas, event) {
-    const rect = canvas.getBoundingClientRect()
-    mouseX = event.clientX - rect.left
-    mouseY = event.clientY - rect.top
+  if (down) {
+    const rect = canvas.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+  context.fillRect(mouseX, mouseY, 5, 5);
+  }
 }
-
 
 //driver code
 init(); //idk
