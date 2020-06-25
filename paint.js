1ea9b8ec59165f75ce7b715f00091f59063c86ed
Catherine Gee <catherinelgee@gmail.com>
2020-06-25 00:29:15 -0700

    bug fixes, line now continuous and user can clear on the more page

--- paint.js
+++ paint.js
@@ -4,9 +4,12 @@ var screenHeight;
 var canvas;
 var context;
 var mouse;
+var prevX;
+var prevY;
 var mouseX;
 var mouseY;
 var down = false;
+var fresh = true;
 
 function init() {
   canvas = document.getElementById("myCanvas");
@@ -25,39 +28,97 @@ function init() {
   context.font = "36px Roboto";
   context.textAlign = "center";
   context.fillText(
-    "click to start painting",
+    "paint: click/drag; clear: crtl+alt+n",
     screenWidth / 2,
     screenHeight / 2
   );
+
+
+
 
   run();
 }
 
 function run() {
-  canvas.addEventListener("mousedown", isDown);
+  context.lineWidth = 1;
+  canvas.addEventListener("mousedown", function(e) {
+    isDown(e);
+    context.beginPath();
+    context.moveTo(e.offsetX, e.offsetY);
+  });
   canvas.addEventListener("mousemove", function(e) {
-    getCursorPosition(canvas, e);
+    draw(e);
   });
   canvas.addEventListener("mouseup", isDown); //this might not work
+
+  window.addEventListener("keydown", function(e) {
+    clearCanvas(canvas, e);
+  });
 }
 
-function isDown() {
-  down = !down;
+function isDown(event) {
+  if(event.button == 0) {
+    down = !down;
+  }
 }
 
+//ITERATION 1
 function getCursorPosition(canvas, event) {
   if (down) {
     const rect = canvas.getBoundingClientRect();
   mouseX = event.clientX - rect.left;
   mouseY = event.clientY - rect.top;
-  context.fillRect(mouseX, mouseY, 5, 5);
+  context.fillRect(mouseX, mouseY, 1, 1);
   }
 }
 
+//ITERATION 2
+function drawLines(canvas, event) {
+  if (down) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+
+    if (fresh) {
+      prevX = mouseX;
+      prevY = mouseY;
+    }
+
+    context.beginPath();
+    context.moveTo(prevX, prevY);
+    context.lineTo(mouseX, mouseY);
+    context.stroke();
+
+    prevX = mouseX;
+    prevY = mouseY;
+  }
+}
+
+//ITERATION 3
+function draw(e){
+  if(!down) return;
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+}
+
+
+function clearCanvas(canvas, event) {
+  console.log("something is clicked");
+  if(event.code == "KeyN" && event.altKey && event.ctrlKey) { //does it work like this??
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "48px Roboto";
+    context.textAlign = "center";
+    context.fillText("PAINT THE SITE", screenWidth / 2, screenHeight / 2 - 30);
+
+    context.font = "36px Roboto";
+    context.textAlign = "center";
+    context.fillText(
+      "paint: click/drag; clear: crtl+alt+n",
+      screenWidth / 2,
+      screenHeight / 2
+    );
+    }
+}
+
 init();
-//driver
-//var f = new FontFace("Roboto", );
-//  f.load().then(function() {
-    // Ready to use the font in a canvas context
-//    init();
-//});
